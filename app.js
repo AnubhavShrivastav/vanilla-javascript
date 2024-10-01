814c3d7d6c7f437048d30edeba574857663c5cc6
anubhavshrivastav <shrivastavanubhav0@gmail.com>
2024-10-01 17:24:15 +0530

    add button

---
+++ app.js
@@ -0,0 +1,26 @@
+//alert("hello world")
+
+//ex-2
+//input
+//let userName = prompt("Enter your userName")
+
+//processing
+//let welcome = "welcome " + userName;
+
+//output
+//alert(welcome)
+
+//const password = Number(prompt("enter your password"));
+//alert(password)
+
+//ex-3
+
+const button = document.querySelector('.btn').addEventListener("click",(e) => {
+    console.log("clicked")
+})
+
+
+
+
+
+
