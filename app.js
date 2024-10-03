eede95cc4ae695a559b3bc3be24c3ce70f246614
anubhavshrivastav <shrivastavanubhav0@gmail.com>
2024-10-03 10:33:30 +0530

    giving output text

--- app.js
+++ app.js
@@ -1,36 +1,15 @@
-//alert("hello world")
+const txtInput = document.querySelector("textarea[name = 'txt-input']")
+const button = document.querySelector('.btn-translate').addEventListener('click',clickhandler)
+const outputText = document.querySelector('#output')
 
-//ex-2
-//input
-//let userName = prompt("Enter your userName")
-
-//processing
-//let welcome = "welcome " + userName;
-
-//output
-//alert(welcome)
-
-//const password = Number(prompt("enter your password"));
-//alert(password)
-
-//ex-3
-
-const button = document.querySelector('.btn').addEventListener('click',clickhandler)
-
-
-//ex-4
-const textarea = document.querySelector('textarea[name = "translator"]')
 
 function clickhandler(){
     // console.log('clicked')
     // console.log("input:",textarea.value)
-    outputText.innerText = "dev " + textarea.value
+    outputText.innerText = "hi! " + txtInput.value;
 }
 
 
-//ex-5
-
-const outputText = document.querySelector('#output')
 
 
 
