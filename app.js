4b6c95ceb4c6173e00ad7f5572479fb32d3e141b
anubhavshrivastav <shrivastavanubhav0@gmail.com>
2024-10-05 17:49:12 +0530

    add URL

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const txtInput = document.querySelector("textarea[name = 'txt-input']")
 const button = document.querySelector('.btn-translate').addEventListener('click',clickhandler)
 const outputText = document.querySelector('#output')
 
+let serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+
+function translationURL(text){
+    return serverURL + "?" + "text=" + text 
+}
 
 function clickhandler(){
     // console.log('clicked')
